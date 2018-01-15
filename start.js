c71007f88f52e49d435833175ab1dfd2372ad851
mattNat <mattsanpedro76@gmail.com>
2018-01-15 13:02:32 -0800

    mod start.js step 6

--- start.js
+++ start.js
@@ -1,1 +1,21 @@
 console.log('Hi, my name is Chris and I\'m 29 years old');
+
+
+function yearOfBirth(age) {
+  try {
+    if (age < 0) throw "is empty";
+  } catch (err) {
+    console.log('Error!');
+    ;
+  }
+  var yearOfBirth = 2018 - age;
+  return yearOfBirth;
+}
+yearOfBirth(-5);
+
+function whoAmI(name, age) {
+  var yob = yearOfBirth(age);
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yob}`);
+}
+whoAmI('Matt', 30);
